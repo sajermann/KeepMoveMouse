d0d9e3561deb97ac37d9d538602c9e50437a53f1
Bruno Sajermann <sajermannbruno@gmail.com>
2022-10-06 16:01:16 -0300

    Add project files.

---
+++ KeepMoveMouse/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Iniciando o KeepMoveMouse, by Sajermann");
+[DllImport("user32.dll")]
+static extern bool SetCursorPos(int X, int Y);
+
+while (true)
+{
+  SetCursorPos(0, 0);
+  await Task.Delay(100);
+  SetCursorPos(500, 500);
+  await Task.Delay(2000);
+}
