000d3bc55d776c9153a378443e95a7b367a01b38
Bruno Sajermann <sajermannbruno@gmail.com>
2022-10-06 16:26:56 -0300

    add simulation click

--- KeepMoveMouse/Program.cs
+++ KeepMoveMouse/Program.cs
@@ -5,10 +5,26 @@ Console.WriteLine("Iniciando o KeepMoveMouse, by Sajermann");
 [DllImport("user32.dll")]
 static extern bool SetCursorPos(int X, int Y);
 
+[System.Runtime.InteropServices.DllImport("user32.dll")]
+static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+const int MOUSEEVENTF_LEFTDOWN = 0x02;
+const int MOUSEEVENTF_LEFTUP = 0x04;
+
+//This simulates a left mouse click
+static void LeftMouseClick(int xpos, int ypos)
+{
+  SetCursorPos(xpos, ypos);
+  mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+  mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+}
+
+
 while (true)
 {
   SetCursorPos(0, 0);
-  await Task.Delay(100);
+  await Task.Delay(2000);
   SetCursorPos(500, 500);
+  LeftMouseClick(500, 500);
   await Task.Delay(2000);
 }
